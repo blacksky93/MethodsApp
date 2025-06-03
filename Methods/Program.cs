9aba03af8768097c946e321455a60dd9a5c20519
mgapp <mgapp@MIKEGYO>
2025-06-03 18:18:12 -0400

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,20 @@
+﻿using Methods;
+
+namespace MethodsApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Test");
+        //SampleMethods.HelloWorld();
+
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    SampleMethods.HelloWorld();
+        //}
+        string str = SampleMethods.SayHi();
+        // DRY Don't repeat yourself. Don't repeat logic.
+        Console.WriteLine(str);
+    }
+}
---
+++ Methods/SampleMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Methods;
+
+public static class SampleMethods
+{
+    public static void HelloWorld ()
+    {
+        Console.WriteLine("Hello World!");
+    }
+
+    public static string SayHi()
+    {
+        return "Hello World!";
+    }
+}
+
